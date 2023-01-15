84273ca636744a899f8bf21f33940ea1c63142a3
Foxtam <foxtam7@ya.ru>
2023-01-15 12:49:57 +0300

    init

---
+++ src/main/java/net/foxtam/hyperskillorg/recipes/Recipes2Application.java
@@ -0,0 +1,13 @@
+package net.foxtam.hyperskillorg.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Recipes2Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/foxtam/hyperskillorg/recipes/Recipes2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.foxtam.hyperskillorg.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipes2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
