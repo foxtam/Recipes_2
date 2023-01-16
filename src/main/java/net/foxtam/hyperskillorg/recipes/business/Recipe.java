9e83103a846a1b0f9ed8312a97c686575b7accfe
Foxtam <foxtam7@ya.ru>
2023-01-16 12:59:07 +0300

    make class Recipe Entity

--- src/main/java/net/foxtam/hyperskillorg/recipes/business/Recipe.java
+++ src/main/java/net/foxtam/hyperskillorg/recipes/business/Recipe.java
@@ -1,13 +1,45 @@
 package net.foxtam.hyperskillorg.recipes.business;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(exclude = {"ingredients", "directions"})
+@Entity
+@Table(name = "recipes")
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @JsonIgnore
+    private long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
-    private List<String> ingredients;
-    private List<String> directions;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "ingredients",
+            joinColumns = @JoinColumn(name = "recipe_id"))
+    @Column(name = "ingredient")
+    private List<String> ingredients = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(
+            name = "directions",
+            joinColumns = @JoinColumn(name = "recipe_id"))
+    @Column(name = "direction")
+    private List<String> directions = new ArrayList<>();
 }
